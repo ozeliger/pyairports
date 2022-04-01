f611ee5a5a82b4e98b22641bb99693d862c802e4
Ozel Iger <84307150+ozeliger@users.noreply.github.com>
2022-04-01 22:38:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,35 @@
 from setuptools import find_packages, setup
-from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+HERE = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, "VERSION"), encoding="utf-8") as f:
-    version = f.read().strip()
+version = "2.1.1"
 
 setup(
     name="pyairports",
-
     version=version,
-
     description="Airport and other locations database",
     long_description=long_description,
-
-    url="https://",
-
-    author="N/A",
-    author_email="N/A",
-
+    url="https://github.com/ozeliger/pyairports",
+    author="Jason Dsouza",
     license="Apache Software License",
-
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-
-        "Intended Audience :: Developers, Data Scientists",
-        "Topic :: Software :: Graph Analysis",
-
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-
     keywords="Airports",
-
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
     # Requirements go here
     install_requires=[],
-
     # Requrements for specific build cycles go here
     # { 'build': [requirements,for,build] }
     extras_require={},
